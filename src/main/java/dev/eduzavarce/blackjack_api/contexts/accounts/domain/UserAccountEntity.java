24bb28269a5b6d96be309fd7ff9b7be33b07ff41
Eduardo Zavarce <eduzavarce@gmail.com>
2025-07-17 13:56:20 +0200

    debug

--- src/main/java/dev/eduzavarce/blackjack_api/contexts/accounts/domain/UserAccountEntity.java
+++ src/main/java/dev/eduzavarce/blackjack_api/contexts/accounts/domain/UserAccountEntity.java
@@ -5,8 +5,7 @@ import dev.eduzavarce.blackjack_api.contexts.shared.domain.Entity;
 public interface UserAccountEntity extends Entity<UserAccount> {
 
   static UserAccountEntity fromDomain(UserAccount userAccount) {
-    throw new UnsupportedOperationException(
-        "This method should be implemented by concrete classes");
+    return dev.eduzavarce.blackjack_api.contexts.accounts.infrastructure.UserAccountMySqlEntity.fromDomain(userAccount);
   }
 
   UserAccount toDomain();
